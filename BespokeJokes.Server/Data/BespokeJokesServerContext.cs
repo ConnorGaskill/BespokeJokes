36d8d8328240c6b23f65899ba229d249249326c5
Connor Gaskill <gaskillconnor@gmail.com>
2025-03-10 16:42:51 -0700

    Add project files.

---
+++ BespokeJokes.Server/Data/BespokeJokesServerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BespokeJokes.Server.Models;
+
+namespace BespokeJokes.Server.Data
+{
+    public class BespokeJokesServerContext : DbContext
+    {
+        public BespokeJokesServerContext (DbContextOptions<BespokeJokesServerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BespokeJokes.Server.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ BespokeJokes.Server/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BespokeJokes.Server.Models
+{
+    public class Joke
+    {
+        [Key]
+        public int JokeId { get; set; }
+        public string JokeText { get; set; }
+        public string PunchLineText { get; set; }
+        public string JokeAuthor { get; set; }
+        public DateTime JokePostDate { get; set; }
+        public DateTime JokeUpdateDate { get; set; }
+
+    }
+}
